d633846d1cc2d2e413743525ce5b7c3be668f685
Paweł Placzyński <placzynski.pawel@gmail.com>
2020-03-21 14:32:41 +0100

    add file saving

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import System.Environment
+import System.IO
 import Vector
 import Geometry
 import Camera
@@ -7,6 +9,12 @@ import Camera
 camera   = Camera (Vector 0 5 0) (Vector 1 (-1) 1) (Screen 640 480)
 triangle = Triangle (Vector 1 0 1) (Vector 2 0 5) (Vector 5 0 2)
 gs       = [triangle]
+pbm      = render camera gs
 
 main :: IO ()
-main = putStrLn $ render camera gs
+main = do
+  args <- getArgs
+  if null args
+    then putStrLn pbm
+    else writeFile (head args) pbm
+  return ()
