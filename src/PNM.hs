7dd5e26ac781357f2e04e0ede51d81bf84c110bc
Paweł Placzyński <placzynski.pawel@gmail.com>
2020-03-25 11:45:54 +0100

    fix rendering for multiple file formats

--- src/PNM.hs
+++ src/PNM.hs
@@ -5,32 +5,31 @@ import Text.Wrap
 import Geometry
 import Screen
 
-data PNMHeader = P1 | P3 deriving Show
+data PNMHeader = P2 | P3 deriving Show
 
 -- | PNM structure.
 data PNM a = PNM { pnmHeader :: PNMHeader
+                 , pnmMax    :: Int
                  , pnmScreen :: Screen
-                 , pnmData :: [a]
+                 , pnmData   :: [a]
                  }
 
 -- | Conversion of PNM data to string.
 pnmString :: (PNMPixel a) => PNM a -> T.Text
 pnmString pnm = wrapText (WrapSettings False False) 70 result
   where result  = T.intercalate (T.pack " ") pixels
-        pixels  = fmap pnmPixel (pnmData pnm)
+        pixels  = fmap (pnmPixel (pnmMax pnm)) (pnmData pnm)
 
 class PNMPixel a where
-  pnmPixel :: a -> T.Text
-
-instance PNMPixel Int where
-  pnmPixel = T.pack . show
+  pnmPixel :: Int -> a -> T.Text
 
 instance PNMPixel (Intersection f a) where
-  pnmPixel (Inter Nothing) = T.pack "0"
-  pnmPixel _               = T.pack "1"
+  pnmPixel mp (Inter Nothing) = T.pack "0"
+  pnmPixel mp _               = T.pack . show $ max mp 65535
 
 instance (PNMPixel a) => Show (PNM a) where
-  show pnm = unlines $ [header, size, body]
+  show pnm = unlines [header, size, maxPx, body]
     where header = show . pnmHeader $ pnm
           size   = show . pnmScreen $ pnm
+          maxPx  = show . pnmMax $ pnm
           body   = T.unpack . pnmString $ pnm
